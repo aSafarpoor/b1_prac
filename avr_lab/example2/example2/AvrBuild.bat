b919de22cf9b732c25985d18b8c1f95de793916c
aSafarpoor <alisafarpoor1108@gmail.com>
2019-10-24 18:32:52 +0330

    recent assembly programs in avr_lab

---
+++ avr_lab/example2/example2/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "D:\98-99-01\15\example2\example2\labels.tmp" -fI -W+ie -C V3 -o "D:\98-99-01\15\example2\example2\example2.hex" -d "D:\98-99-01\15\example2\example2\example2.obj" -e "D:\98-99-01\15\example2\example2\example2.eep" -m "D:\98-99-01\15\example2\example2\example2.map" "D:\98-99-01\15\example2\example2\example2.asm"
